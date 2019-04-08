b7f2f198ba9ab0cfffdb080da413a1cc12a7db0d
Sachin Kasana <sachin7.kumar@airtel.com>
2019-04-08 21:00:19 +0530

    code updated

--- src/app/hotel/hotel-list/hotel-list.component.ts
+++ src/app/hotel/hotel-list/hotel-list.component.ts
@@ -15,23 +15,11 @@ export class HotelListComponent implements OnInit {
     hotelName: '',
     location: ''
   }
-  filter;
+
   ngOnInit() {
     this._hotelService.getHotelsList().subscribe(res => {
       this.hotelsList = res;
-      console.log('res', res);
-    })
-  }
-  
-
-  searchHotels() {
-    console.log('search',this.hotelFilter);
-  this.filter=this.hotelFilter;
-    // this.hotelsList= this.hotelsList.filter((hotel)=>{
-    //   debugger
-    //  return hotel.name.toLowerCase()==this.hotelFilter.hotelName.toLowerCase()
-    //         || this.hotelFilter.location.toLowerCase()==hotel.Addrees.toLowerCase();
-    // })
+    });
   }
 
 }
